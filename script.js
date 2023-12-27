b088011ae2a8cca91cf07b7ee3a5cb3fa13cad2f
Shavandeb Kaiti <91772809+Emulsion24@users.noreply.github.com>
2023-12-28 00:46:20 +0530

    Update script.js
    add green color effect on right target hit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//create no of bubble
 function makebuble(){
 var bubleCount=``;
 for(var i=0;i<=159;i++){
@@ -8,67 +8,68 @@ for(var i=0;i<=159;i++){
 }
 document.querySelector("#pnlbtm").innerHTML=bubleCount;
 }
-
-var timer=60;
+//add timer to the game
+var timer=10;
 function countTimer(){
     var time=setInterval(() => {
         if(timer>0){
             timer--;
             document.querySelector("#timerval").textContent=timer;
-         }
+        }
         else
         {
         clearInterval(time);
         document.querySelector("#pnlbtm").innerHTML=`<div id="gameover"><h1>Game Over<h1>
-        <input type="button" value="Play Again" onClick="window.location.reload()">
+        <input type="button" value="Play Again" id="playagain" onClick="window.location.reload()">
         <div>`
-
+        
         }
-
-       
     }, 1000);
     
 }
+//genarate target number
 var hit=0;
 function hited(){
+    if (timer!=0){
      hit=Math.floor(Math.random()*10);
     document.querySelector("#hitter").textContent=hit;
+    }
+    else
+    document.querySelector("#hitter").textContent=0;
 }
+//add score
 var score=0;
 function updateScore(){
     score+=10;
 document.querySelector("#scoreval").textContent=score;
 }
+//deduct score
 function less(){
     score-=10;
 document.querySelector("#scoreval").textContent=score;
 
 }
-function changecolor(){
-
-}
-
 
 makebuble();
 countTimer();
 hited();
+//check the hitted value with target value
 document.querySelector("#pnlbtm").addEventListener("click",function(dls){
     var get=Number(dls.target.textContent);
     if(hit==get){
+        dls.target.style.backgroundColor="#39e75f";
+        setTimeout(() => {
         
         updateScore();
         hited();
         makebuble();
-        dls.target.style.backgroundColor="green";
+        
+        },100);
     }
     else if(hit>get||hit<get){
         less();
         dls.target.style.backgroundColor="red";
     }
-
-
     
-    
-
 });
 
